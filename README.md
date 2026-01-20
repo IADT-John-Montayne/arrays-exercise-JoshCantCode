575a8360449b11a7187ec57e97c03e4e7923e8cf
Josh <joshpatg2005@gmail.com>
2026-01-20 12:21:48 +0000

    Examples done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/SUz8Ddfj)
+
+
+
+The Translate and Transform functions dont apply to the current object you're drawing, but the entire canvas, affecting all future objects.
--- sketch.js
+++ sketch.js
@@ -1,6 +1,46 @@
+// Change the value inside an array given the length and specific index
+let names = ['John', 'David', 'Mary'];
+console.log(names);
+
+names[1] = 'Brian';
+console.log(names);
+console.log("====================")
+
+// Add an item to the end of an array
+console.log(`Length (before): ${names.length}`)
+names.push('Josh');
+console.log(names);
+console.log(`Length (after): ${names.length}`)
+console.log("====================")
+
+// Remove the last item in an array
+names.pop();
+console.log(names);
+console.log("====================")
+
+// Add a new item at the beginning of an array
+names.unshift('Josh');
+console.log(names);
+console.log("====================")
+// Remove the first item in an array
+names.shift();
+console.log(names);
+console.log("====================")
+
+// Loop through an array
+
+for(let i = 0; i < names.length; i++) {
+	console.log(names[i]);
+}
+
+// Alterantively
+console.log(names.join(" "))
+console.log("====================")
+
+
 function setup() {
 	createCanvas(500, 500);
-	background(55);
+	background(30);
 	rectMode(CENTER);
 }
 
